7e964c413cdf566a23f6b36771fafc74b1f58deb
Harsh Agrawal <helpfromharsh@gmail.com>
2025-04-18 20:55:14 +0530

    Product image fixes

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,22 +6,22 @@ export default function Products() {
       <h2 className="text-3xl font-semibold text-blue-700 mb-6">Our Products</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <div className="border p-4 shadow hover:shadow-lg transition">
-          <img src="/Whitephenyle.jpg" alt="Product 1" className="w-full h-48 object-cover mb-4"/>
+          <img src="/Whitephenyle.jpg" alt="Product 1" className="w-full max-h-60 object-contain mb-4"/>
           <h3 className="text-xl font-bold">White Phenyle (1L)</h3>
           <p>Strong disinfectant with a long-lasting fragrance.</p>
         </div>
         <div className="border p-4 shadow hover:shadow-lg transition">
-          <img src="/blackphenyle.jpg" alt="Product 2" className="w-full h-48 object-cover mb-4"/>
+          <img src="/blackphenyle.jpg" alt="Product 2" className="w-full max-h-60 object-contain  mb-4"/>
           <h3 className="text-xl font-bold">Black Phenyle (5L)</h3>
           <p>Industrial-grade cleaner for deep sanitation.</p>
         </div>
         <div className="border p-4 shadow hover:shadow-lg transition">
-          <img src="/broom.jpg" alt="Product 2" className="w-full h-48 object-cover mb-4"/>
+          <img src="/broom.jpg" alt="Product 2" className="w-full max-h-60 object-cover mb-4"/>
           <h3 className="text-xl font-bold">Broom</h3>
           <p>Magic Broom for all kinds of household and commercial cleaning.</p>
         </div>
         <div className="border p-4 shadow hover:shadow-lg transition">
-          <img src="/wiper.jpg" alt="Product 2" className="w-full h-48 object-cover mb-4"/>
+          <img src="/wiper.jpg" alt="Product 2" className="w-full max-h-60 object-cover mb-4"/>
           <h3 className="text-xl font-bold">Wiper</h3>
           <p>Wiper for all kinds of water and chemical cleaning from surface.</p>
         </div>
